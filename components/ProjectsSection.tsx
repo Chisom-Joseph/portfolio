1cfa9b7b844e9276e65cb887605eef47fc60e0e0
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-11-10 18:23:17 +0100

    Used AnimatedNav in ProjectsSection

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,18 +1,14 @@
 "use client";
-import { useState, useRef, useLayoutEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import AOS from "aos";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import Wrapper from "@/components/Wrapper";
-import ProjectCard from "./ProjectCard";
 import { projects } from "@/data";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 import ButtonOutlineCustom from "./ui/custom/ButtonGradient";
 import { FaArrowRight } from "react-icons/fa6";
+import AnimatedNav from "@/components/ui/custom/AnimatedNav";
 
 export default function ProjectsSection() {
-  const navListRef = useRef<HTMLDivElement | null>(null);
-  const activeCategoryRef = useRef<HTMLDivElement | null>(null);
   const [projectCategories, setProjectCategories] = useState<Set<string>>(
     new Set(["All"])
   );
@@ -30,71 +26,24 @@ export default function ProjectsSection() {
     AOS.refresh();
   }, [activeCategory]);
 
-  useGSAP(
-    () => {
-      gsap.to(navListRef.current, { opacity: 1, duration: 1 });
-    },
-    { scope: navListRef }
-  );
-
-  const handleCategoryChange = ({
-    category,
-    index,
-  }: {
-    category: string;
-    index: number;
-  }) => {
-    if (navListRef.current && activeCategoryRef.current) {
-      const activeItem = navListRef.current.children[index] as HTMLElement;
-
-      gsap.to(activeCategoryRef.current, {
-        duration: 0.05,
-        width: activeItem.offsetWidth,
-        left: activeItem.offsetLeft,
-        ease: "power2.inOut",
-      });
-
-      setActiveCategory(category);
-    }
-  };
-
   return (
-    <section id="about" className="mt-[8em] xl:mt-[12em] mb-[8em]">
+    <section id="about" className="mb-[8em] mt-[8em] xl:mt-[12em]">
       <Wrapper>
         <h1
           data-aos="fade-down"
-          className="font-powerGroteskUltraBold text-[3em] md:text-[4em] w-fit leading-[0.9em] md:leading-[1em] mb-[0.5em] mt-[0.2em] m-auto"
+          className="m-auto mb-[0.5em] mt-[0.2em] w-fit font-powerGroteskUltraBold text-[3em] leading-[0.9em] md:text-[4em] md:leading-[1em]"
         >
           Projects
         </h1>
 
-        <div
-          ref={navListRef}
-          data-aos="flip-up"
-          data-aos-duration="5000"
-          className="relative font-bold flex max-h-[4em] first-child:text-red-300 items-center justify-around w-max m-auto mb-[4em] rounded-full border-[1px] dark:bg-white/5"
-        >
-          {Array.from(projectCategories).map((category, index) => (
-            <div
-              key={index}
-              className={`${
-                activeCategory === category && "text-white"
-              } md:text-[1em] text-[0.7em] py-4 relative flex items-center px-[2em] h-full cursor-pointer transition-all duration-500 ease-in-out`}
-              onClick={() => handleCategoryChange({ category, index })}
-            >
-              {index == 0 && (
-                <div
-                  ref={activeCategoryRef}
-                  className="categoryActive h-full w-full absolute top-0 left-0 bg-gradient rounded-full -z-10 transition-all duration-300 ease-in-out"
-                />
-              )}
-              {category}
-            </div>
-          ))}
-        </div>
+        <AnimatedNav
+          items={Array.from(projectCategories)}
+          activeItem={activeCategory}
+          onCategoryChange={setActiveCategory}
+        />
 
         <div data-aos="zoom-in-up">
-          <BentoGrid className="max-w-4xl mx-auto mb-16">
+          <BentoGrid className="mx-auto mb-16 max-w-4xl">
             {projects.map((project, index) => (
               <BentoGridItem
                 key={index}
