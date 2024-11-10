de04df251ced2ec0f465461f37d887f5ade68ede
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2024-11-10 01:30:59 +0100

    Made AnimatedNave a reuseable component and used it in SkillSection

--- components/SkillsSection.tsx
+++ components/SkillsSection.tsx
@@ -1,14 +1,11 @@
 "use client";
-
-import { useState, useRef, useEffect, useLayoutEffect } from "react";
-import gsap from "gsap";
+import { useState, useLayoutEffect } from "react";
 import { skills } from "@/data";
 import Wrapper from "./Wrapper";
 import AOS from "aos";
+import CategoryNav from "@/components/ui/custom/AnimatedNav";
 
 export default function SkillsSection() {
-  const navListRef = useRef<HTMLDivElement | null>(null);
-  const activeSkillRef = useRef<HTMLDivElement | null>(null);
   const [skillCategories, setSkillCategories] = useState<Set<string>>(
     new Set(["All"])
   );
@@ -26,65 +23,25 @@ export default function SkillsSection() {
     AOS.refresh();
   }, [activeSkill]);
 
-  const handleCategoryChange = ({
-    skill,
-    index,
-  }: {
-    skill: string;
-    index: number;
-  }) => {
-    if (navListRef.current && activeSkillRef.current) {
-      const activeItem = navListRef.current.children[index] as HTMLElement;
-
-      gsap.to(activeSkillRef.current, {
-        duration: 0.05,
-        width: activeItem.offsetWidth,
-        left: activeItem.offsetLeft,
-        ease: "power2.inOut",
-      });
-
-      setActiveSkill(skill);
-    }
-  };
-
   return (
     <section className="mb-[8em]">
       <Wrapper>
         <h1
           data-aos="fade-down"
-          className="font-powerGroteskUltraBold text-[3em] md:text-[4em] w-fit leading-[0.9em] md:leading-[1em] mb-[0.5em] mt-[0.2em] m-auto"
+          className="m-auto mb-[0.5em] mt-[0.2em] w-fit font-powerGroteskUltraBold text-[3em] leading-[0.9em] md:text-[4em] md:leading-[1em]"
         >
           Skills
         </h1>
 
-        <div
-          ref={navListRef}
-          data-aos="flip-up"
-          data-aos-duration="5000"
-          className="relative font-bold flex max-h-[4em] first-child:text-red-300 items-center justify-around w-max m-auto mb-[4em] rounded-full border-[1px] dark:bg-white/5"
-        >
-          {Array.from(skillCategories).map((skill, index) => (
-            <div
-              key={index}
-              className={`${
-                activeSkill === skill && "text-white"
-              } md:text-[1em] text-[0.7em] py-4 relative flex items-center px-[2em] h-full cursor-pointer transition-all duration-500 ease-in-out`}
-              onClick={() => handleCategoryChange({ skill, index })}
-            >
-              {index == 0 && (
-                <div
-                  ref={activeSkillRef}
-                  className="categoryActive h-full w-full absolute top-0 left-0 bg-gradient rounded-full -z-10 transition-all duration-300 ease-in-out"
-                />
-              )}
-              {skill}
-            </div>
-          ))}
-        </div>
+        <CategoryNav
+          items={Array.from(skillCategories)}
+          activeItem={activeSkill}
+          onCategoryChange={setActiveSkill}
+        />
 
         <div
           data-aos="zoom-out-up"
-          className="max-w-4xl mx-auto mb-16 flex items-center justify-center flex-wrap gap-6"
+          className="mx-auto mb-16 flex max-w-4xl flex-wrap items-center justify-center gap-6"
         >
           {skills.map((skill, index) => (
             <div
@@ -96,7 +53,7 @@ export default function SkillsSection() {
                   : "hidden"
               } hover:scale-110 transition-all cursor-pointer text-body text-center`}
             >
-              <div className="text-6xl p-4 border-[1px] border-body rounded-[0.55em] w-fit mb-4">
+              <div className="mb-4 w-fit rounded-[0.55em] border-[1px] border-body p-4 text-6xl">
                 <skill.icon />
               </div>
               <p>{skill.title}</p>
---
+++ components/ui/custom/AnimatedNav.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { FC, useRef } from "react";
+import gsap from "gsap";
+
+type AnimatedNavProps = {
+  items: any[];
+  activeItem: string;
+  onCategoryChange: (item: string) => void;
+};
+
+const AnimatedNav: FC<AnimatedNavProps> = ({
+  items,
+  activeItem,
+  onCategoryChange,
+}) => {
+  const navListRef = useRef<HTMLDivElement | null>(null);
+  const activeItemRef = useRef<HTMLDivElement | null>(null);
+
+  const handleCategoryChange = (item: string, index: number) => {
+    if (navListRef.current && activeItemRef.current) {
+      const activeItem = navListRef.current.children[index] as HTMLElement;
+
+      gsap.to(activeItemRef.current, {
+        duration: 0.05,
+        width: activeItem.offsetWidth,
+        left: activeItem.offsetLeft,
+        ease: "power2.inOut",
+      });
+
+      onCategoryChange(item);
+    }
+  };
+
+  return (
+    <div
+      ref={navListRef}
+      data-aos="flip-up"
+      data-aos-duration="5000"
+      className="relative m-auto mb-[4em] flex max-h-[4em] w-max items-center justify-around rounded-full border-[1px] font-bold dark:bg-white/5"
+    >
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className={`${
+            activeItem === item && "text-white"
+          } md:text-[1em] text-[0.7em] py-4 relative flex items-center px-[2em] h-full cursor-pointer transition-all duration-500 ease-in-out`}
+          onClick={() => handleCategoryChange(item, index)}
+        >
+          {index === 0 && (
+            <div
+              ref={activeItemRef}
+              className="categoryActive bg-gradient absolute left-0 top-0 -z-10 h-full w-full rounded-full transition-all duration-300 ease-in-out"
+            />
+          )}
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedNav;
